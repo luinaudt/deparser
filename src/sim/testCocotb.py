8ac50292049c4c1c5c80ef252549793fd8c5ef50
luinaud thomas <luinaud@localhost.localdomain>
2019-10-02 15:42:35 -0400

    global test

--- src/sim/testCocotb.py
+++ src/sim/testCocotb.py
@@ -1,14 +1,18 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.clock import Clock
 
 @cocotb.test()
 def testCocotb(dut):
     """Try accessing the design."""
+    cocotb.fork(Clock(dut.clk,10,'ns').start())
+    dut._log.info("Running test!")
+    val1=0
+    val2=0
+    for val1 in range(256):
+        dut.valueIn1 <= val1
+        for val2 in range(256):
+            dut.valueIn2 <= val2
+            yield RisingEdge(dut.clk)
+    dut._log.info("Running test!")
 
-    dut._log.info("Running test!")
-    for cycle in range(10):
-        dut.clk = 0
-        yield Timer(1000)
-        dut.clk = 1
-        yield Timer(1000)
-    dut._log.info("Running test!")
