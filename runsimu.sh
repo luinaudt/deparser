5c9d1e5170c83fca4100f49f328f38b42c44ad4d
Thomas Luinaud <thomas.luinaud@gmail.com>
2020-03-04 15:20:12 -0500

    docker v1_3

--- runsimu.sh
+++ runsimu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-singularity run cocotb_v1.3.simg ./.run_sim.sh $@
-#docker run --rm -it -v $PWD:/home/workspace cocotb:v1 ./.run_sim.sh $@
+#singularity run cocotb_v1.3.simg ./.run_sim.sh $@
+docker run --rm -it -v $PWD:/home/workspace cocotb:v1_3 ./.run_sim.sh $@
 
 
