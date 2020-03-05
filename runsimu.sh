e6da1c4d52dd84097256d2cd9bbd3f8280c56136
Thomas Luinaud <thomas.luinaud@gmail.com>
2020-03-05 11:07:54 -0500

    support to simulate on grm machines

--- runsimu.sh
+++ runsimu.sh
@@ -1,5 +1,35 @@
-#!/bin/bash
-singularity run cocotb_v1.3 ./.run_sim.sh $@
+#!/bin/tcsh
+
+## virtual env
+#singularity run cocotb_v1.3 ./.run_sim.sh $@
 #docker run --rm -it -v $PWD:/home/workspace cocotb:v1 ./.run_sim.sh $@
 
+## GRM
+if (! -d ".env_cocotb") then
+    printf "cocotb virtualenv directory does not exist\n"
+    if (`where virtualenv` == "") then
+	printf "virtualenv command doesn't not exist\n"
+	printf "Install it using \n"
+	printf "\t pip3 install --user virtualenv \n"
+	printf "and add it to your path : \n"
+	printf "\t echo 'setenv PATH \044PATH\:/users/$USER/.local/bin/' >> ~/.cshrc \n"
+	printf "reload your env : source ~/.cshrc\n "
+	exit -1
+    else
+	printf "creating directory for virtualenv .env_cocotb\n"
+        mkdir .env_cocotb
+	printf "creating virtualenv in .env_cocotb\n"
+	stpython37
+	virtualenv .env_cocotb
+	printf "installing dependencies in virtual env \n"
+	source ./.env_cocotb/bin/activate.csh
+	# install dependencies
+	pip install cocotb
+	deactivate   # leave virtual env to keep stable script
+    endif
+endif
+stmodelt
+stpython37
+source ./.env_cocotb/bin/activate.csh
+./.run_sim.sh 
 
