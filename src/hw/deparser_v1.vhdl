4f8661eca03288b36aa63421aef780964150711b
Thomas Luinaud <thomas.luinaud@polymtl.ca>
2020-04-23 11:51:03 -0400

    création des mux

--- src/hw/deparser_v1.vhdl
+++ src/hw/deparser_v1.vhdl
@@ -6,7 +6,7 @@
 -- Author     : luinaud thomas  <luinaud@localhost.localdomain>
 -- Company    : 
 -- Created    : 2019-10-02
--- Last update: 2020-03-24
+-- Last update: 2020-04-23
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -22,11 +22,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+use ieee.math_real.log2;
+use ieee.math_real.ceil;
+
 entity deparser is
 
   generic (
-    payloadStreamSize : natural := 256;   -- size of input payload
-    outputStreamSize  : natural := 256;   -- size of output streaming
+    nbInMux           : natural := 8;     -- number of input for each mux
+    payloadStreamSize : natural := 64;    -- size of input payload
+    outputStreamSize  : natural := 64;    -- size of output streaming
     ethsize           : natural := 112;   -- ethernet width
     ipv4size          : natural := 160;   -- IPv4 header size
     tcpSize           : natural := 160);  -- tcp header size
@@ -34,6 +38,7 @@ entity deparser is
   port (
     clk          : in  std_logic;
     rst          : in  std_logic;
+    sel_test     : in  integer range 0 to nbInMux -1;
     ethBus       : in  std_logic_vector(ethsize-1 downto 0);
     ipv4Bus      : in  std_logic_vector(ipv4size - 1 downto 0);
     tcpBus       : in  std_logic_vector(tcpSize - 1 downto 0);
@@ -58,15 +63,75 @@ end entity deparser;
 -- inside the deparser we can buffer part of the packet
 -- state machine for management.
 architecture behavioral of deparser is
+  type mux_t is array (0 to nbInMux - 1) of std_logic_vector(7 downto 0);  --all muxes
+  type muxes_t is array (0 to outputStreamSize/8 - 1) of mux_t;
+  type muxes_o_t is array (0 to outputStreamSize/8 - 1) of std_logic_vector(7 downto 0);
+  type muxes_sel_t is array(0 to outputStreamSize/8 - 1) of integer range 0 to nbInMux - 1;  --control signals
 
+  signal muxes_o : muxes_o_t;           -- all output muxes
+  signal muxes   : muxes_t;             -- all input muxes
+  signal sel     : muxes_sel_t;         -- all selector for muxes
+
+  signal mux0_r : std_logic_vector(7 downto 0);
+  signal mux1_r : std_logic_vector(7 downto 0);
+  signal mux2_r : std_logic_vector(7 downto 0);
+  signal mux3_r : std_logic_vector(7 downto 0);
+  signal mux4_r : std_logic_vector(7 downto 0);
+  signal mux5_r : std_logic_vector(7 downto 0);
+  signal mux6_r : std_logic_vector(7 downto 0);
+  signal mux7_r : std_logic_vector(7 downto 0);
+
+  signal sels     : muxes_sel_t;
+  signal full_hdr : std_logic_vector(ethsize + ipv4size + tcpSize + payloadStreamSize - 1 downto 0);
 begin  -- architecture behavioral
+  full_hdr <= payloadData & tcpBus & ipv4Bus & ethBus;
+  eth_assign : process(full_hdr)
+  begin
+    for i in muxes'range loop
+      for j in muxes(i)'range loop
+        muxes(i)(j) <= full_hdr((j*muxes(i)'high + i)*8 + 7 downto (j*muxes(i)'high + i)*8);
+      end loop;
+    end loop;
+  end process;
 
-  process (clk, rst) is
+  sel_assign : process(sel_test)
+  begin
+    for i in sel'range loop
+      sel(i)<=sel_test;
+    end loop;
+  end process;
+  
+  --! \brief generates all muxes
+  muxes_generation : process(muxes, sel) is
+  begin
+    for i in muxes_o'range loop
+      muxes_o(i) <= muxes(i)(sel(i));
+    end loop;
+  end process;
+
+
+  --! \brief process to clk all muxes output
+  muxes_registration : process(clk) is
+  begin
+    if rising_edge(clk) then
+      mux0_r <= muxes_o(0);
+      mux1_r <= muxes_o(1);
+      mux2_r <= muxes_o(2);
+      mux3_r <= muxes_o(3);
+      mux4_r <= muxes_o(4);
+      mux5_r <= muxes_o(5);
+      mux6_r <= muxes_o(6);
+      mux7_r <= muxes_o(7);
+    end if;
+  end process;
+
+
+  data_out : process (clk, rst) is
   begin  -- process
     if rst = '0' then                   -- asynchronous reset (active low)
       outputData <= (others => '0');
     elsif clk'event and clk = '1' then  -- rising clock edge
-      outputData <= ipv4Bus(outputStreamSize - ethsize - 1 downto 0) & ethBus;
+      outputData <= mux7_r & mux6_r & mux5_r & mux4_r & mux3_r & mux2_r & mux1_r & mux0_r;
     end if;
   end process;
 
--- src/sim/deparser_v1.py
+++ src/sim/deparser_v1.py
@@ -12,11 +12,18 @@ def testAll(dut):
     dut.rst<=0
     dut.ethBus <= 0
     dut.IPv4Bus <= 0
+    dut.payloadData<= 0
+    dut.tcpBus<=0
     yield ClockCycles(dut.clk,10)
     dut.rst<=1
     dut._log.info("end Rst")
     dut.ethBus <= int.from_bytes(raw(Ether(src="12:34:56:78:90:23",dst="10:23:65:78:94:10", type="IPv4")), 'little')
     dut.IPv4Bus <= int.from_bytes(raw(IP(src="192.168.1.1", dst="192.168.1.2")) , 'little')
-    
+    dut.tcpBus <= int.from_bytes(raw(TCP(sport=80, dport=12000)) , 'little')
+    dut.payloadData <= int(0xDEADBEEFDEADBEEF)
+    yield ClockCycles(dut.clk,15)
+    for i in range(8):
+        dut.sel_test<= i;
+        yield ClockCycles(dut.clk,1)
     yield ClockCycles(dut.clk,15)
     dut._log.info("end Test")
